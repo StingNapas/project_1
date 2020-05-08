4315efe5624e4ed496b71343f20428223ed97d84
Sergey <2sting2@mail.ru>
2020-05-08 12:16:20 +0300

    first prj

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 "use strict";
 
-let num = 9991;
-console.log(num);
+let money, time;
+
+money = prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+	budget : +money,
+	timeData : time,
+	expenses : {},
+	optionalExpenses : {},
+	income : [],
+	saving : false,
+};
+
+appData.expenses[prompt("Введите обязательную статью расходов в этом месяце")] = prompt("Во сколько обойдется?");
+alert(appData.budget / 30);
